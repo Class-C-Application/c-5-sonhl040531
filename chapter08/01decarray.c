45e9da047baefad71dbdda7296c710dab06edeab
sonhl040531 <son80538053@gmail.com>
2026-04-01 02:40:59 +0000

    0401

--- chapter08/01decarray.c
+++ chapter08/01decarray.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
-
+#define SIZE 5
 // SIZE라는 이름으로 정수 5를 나타내는 매크로 상수를 정의하세요
 
 int main(void)
 {
 	//배열선언
 	// 매크로 상수 SIZE 크기의 int형 배열 score를 선언하세요
-
+	int score[SIZE];
+	score[0] = 78;
+	score[1] = 97;
+	score[2] = 85;
+	score[4] = 91;
 	//배열 원소에 값 저장
 	// score[0]에 78, score[1]에 97, score[2]에 85를 저장하세요
 	// score[3]에는 값을 저장하지 마세요 (쓰레기 값이 저장됩니다)
 	// score[4]에 91을 저장하세요
 
 	//배열원소 출력
+	for (int i =0; i< SIZE; i++)
+	printf("%d", score[i]);
 	// 0부터 SIZE 미만까지 반복하는 for문을 작성하고, 각 원소를 printf("%d  ", score[i]); 형식으로 출력하세요
 	printf("\n");
 
